94a2301be513abe87ecf4b660f081792e8c9a7ee
King Keys Admin <kingkeysshop@gmail.com>
2026-04-26 10:22:27 -0500

    fix: correct aurpay module export for payment provider

--- backend/src/modules/aurpay/index.ts
+++ backend/src/modules/aurpay/index.ts
@@ -1,5 +1,6 @@
 import AurpayPaymentService from "./services/aurpay-payment"
-import { Module } from "@medusajs/framework/utils"
 
-export const AURPAY_MODULE = "aurpay"
-export default Module(AURPAY_MODULE, { service: AurpayPaymentService })
+
+export default {
+  services: [AurpayPaymentService],
+}

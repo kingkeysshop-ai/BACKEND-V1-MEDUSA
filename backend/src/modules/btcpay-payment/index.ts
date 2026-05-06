18d3b07acfcde3c666d160800ff45048ba3ea554
King Keys Admin <kingkeysshop@gmail.com>
2026-05-06 00:17:04 -0500

    fix: btcpay index exporta directamente el servicio como payment provider

--- backend/src/modules/btcpay-payment/index.ts
+++ backend/src/modules/btcpay-payment/index.ts
@@ -1,8 +1,3 @@
-import { Module } from "@medusajs/framework/utils"
 import BTCPayProviderService from "./services/btcpay-provider.service"
 
-export const BTCPAY_MODULE = "btcpay_payment"
-
-export default Module(BTCPAY_MODULE, {
-  service: BTCPayProviderService,
-})
+export default BTCPayProviderService
